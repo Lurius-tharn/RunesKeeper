0c385ae2c276b71c02d4d4d1787d02cbdeb64278
Caint-dev <Caint-dev@users.noreply.github.com>
2021-11-09 17:17:26 +0100

    Ajout de plusieurs fonctionnalités

--- server/routes/RkRouter.js
+++ server/routes/RkRouter.js
@@ -3,18 +3,19 @@ const router = express.Router();
 
 const libraryController = require('../controller/LibraryController');
 const connectController = require('../controller/connectController');
+const userController = require('../controller/UserController');
 
 //connectController Declaration
-router.post("/signin",connectController.signin);
-router.post("/signup",connectController.signup);
+router.post("/signin", connectController.signin);
+router.post("/signup", connectController.signup);
 
-router.get("/allBooksbysection/:userId",libraryController.AllBooksbysection);
-router.get("/booksbySection/:section/:userId",libraryController.booksbySection);
-router.get("/allBooksbyAuthor/:userId",libraryController.Allbooksbyauthor);
-router.get("/booksbyauthor/:userId/:author",libraryController.Booksbyauthor);
-router.get("/allBooksbyCategories/:userId",libraryController.AllBooksbyCategories);
-router.get("/bookbyIsbn/:userId/:isbn",libraryController.BookbyIsbn);
-router.get("/sectionsofBook/:userId/:isbn",libraryController.SectionsofBook);
+router.get("/allBooksbysection/:userId", libraryController.AllBooksbysection);
+router.get("/booksbySection/:section/:userId", libraryController.booksbySection);
+router.get("/allBooksbyAuthor/:userId", libraryController.Allbooksbyauthor);
+router.get("/booksbyauthor/:userId/:author", libraryController.Booksbyauthor);
+router.get("/allBooksbyCategories/:userId", libraryController.AllBooksbyCategories);
+router.get("/bookbyIsbn/:userId/:isbn", libraryController.BookbyIsbn);
+router.get("/sectionsofBook/:userId/:isbn", libraryController.SectionsofBook);
 
-
+router.post("/newKeeper", userController.newKeeper)
 module.exports = router
