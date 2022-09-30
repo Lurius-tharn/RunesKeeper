c9c4987de5eb235ce9a62f751ae0f432c73c29ad
dsajous <davenese.sajous@soprasteria.com>
2022-09-30 11:40:51 +0200

    ajout  api

--- client/src/API/GoogleBooksApi.js
+++ client/src/API/GoogleBooksApi.js
@@ -8,5 +8,21 @@ const getGoogleBooksApiWithSearchedText  = (text) => {
 }
 
 
+/**
+ * 
+ * 
+ *  intitle: Returns results where the text following this keyword is found in the title.
+    inauthor: Returns results where the text following this keyword is found in the author.
+    inpublisher: Returns results where the text following this keyword is found in the publisher.
+    subject: Returns results where the text following this keyword is listed in the category list of the volume.
+    isbn: Returns results where the text following this keyword is the ISBN number.
+ * 
+ * GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:hobb+isbn:11313131131331&key=yourAPIKey
+ * 
+ * 
+ * orderBy = relevance
+ * langRestrict:fr
+ */
+
 export default getGoogleBooksApiWithSearchedText(name)
 
